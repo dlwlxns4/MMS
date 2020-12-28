a9fb49cfaf2487de69c746e5e56f317b9cb6d9b8
jhLim97 <wnsgur2749@naver.com>
2020-12-28 19:13:22 +0900

    second test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("초기상태입니다");
-        System.out.println("PUSH");
+        junhyukClass jh = new junhyukClass();
     }
 }
---
+++ src/com/company/junhyukClass.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class junhyukClass {
+	
+	public junhyukClass() {
+		System.out.println("PUSH");
+	}
+	
+
+}
